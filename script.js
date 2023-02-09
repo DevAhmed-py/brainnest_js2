33cbb27ddb16e60705ce9d21d1b5d800caf018ad
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2023-02-09 15:37:08 +0100

    done

--- script.js
+++ script.js
@@ -4,25 +4,26 @@ const resultText = document.querySelector(".resultText");
 const scoresText = document.querySelector(".scores");
 const playerChoice = document.querySelectorAll("#btnChoice");
 
-let computerScore = 0
-let playerScore = 0
-let maxScore = 5
-
-let player;
-let computer;
-let result;
+let playerScore = 0;
+let computerScore = 0;
+let maxScore = 5;
 
 playerChoice.forEach(button => button.addEventListener("click", () => {
-    player = button.textContent;
-    computer = computerPlay();
-    playerText.textContent = `Player: ${player}`.toUpperCase();
-    computerText.textContent = `Computer: ${computer}`.toUpperCase();
-    // console.log(playRound(player, computer))
-    resultText.textContent = `Result: ${playRound(player, computer)}`;
-    // resultText.textContent = `Result: ${console.log(playRound(player, computer))}`;
-    // const winner = checkWin(player, computer);
-    // showResult(winner);
-    scoresText.textContent = `Computer: ${computerScore} vs Player: ${playerScore}`
+    if (playerScore >= maxScore || computerScore >= maxScore) {
+        if (playerScore > computerScore) {
+            resultText.textContent = `Result: Game Over! Player WIN`
+        } else {
+            resultText.textContent = `Result: Game Over! Computer WIN`
+        }
+
+    } else {
+        let player = button.innerText;
+        let computer = computerPlay();
+        playerText.textContent = `Player: ${player.toUpperCase()}`;
+        computerText.textContent = `Computer: ${computer.toUpperCase()}`;
+        resultText.textContent = `Result: ${playRound(player, computer)}`;
+        scoresText.textContent = `Computer: ${computerScore} vs Player: ${playerScore}`;
+    }
 }));
 
 function computerPlay() {
@@ -38,98 +39,11 @@ function computerPlay() {
             computer = 'scissors'
             break;
     }
-    return computer
+    return computer;
 }
 
-// function playRound(playerSelection, computerSelection) {
-//     if (playerSelection === computerSelection) {
-//       return "It's a DRAW! Play Again!!";
-
-//     } else if (playerSelection === 'rock') {
-//       if (computerSelection === 'paper') {
-//         computerScore++
-//         return 'Computer';
-//       } else {
-//         playerScore++
-//         return 'Player';
-//       }
-//     } else if (playerSelection === 'paper') {
-//       if (computerSelection === 'scissors') {
-//         computerScore++
-//         return 'Computer';
-//       } else {
-//         playerScore++
-//         return 'Player';
-//       }
-//     } else if (playerSelection === 'scissors') {
-//       if (computerSelection === 'rock') {
-//         computerScore++
-//         return 'Computer';
-//       } else {
-//         playerScore++
-//         return 'Player';
-//       }
-//     }
-// }
-
-// function showResult(winner) {
-//     if (winner === 'player') {
-//         playerScore++;
-//         resultText.textContent = `Result: Player WIN!!!`;
-//     } else if (winner === 'computer') {
-//         computerScore++
-//         resultText.textContent = `Result: Computer WIN!!!`;
-//     } else {
-//         resultText.textContent = `Result: It's a DRAW! Play Again!!!`;
-//     }
-// }
-
-
-// function play(e) {
-//     restart.style.display = 'inline-block';
-//     const playerChoice = e.target.id;
-//     const computerChoice = getComputerChoice();
-//     const winner = checkWin(playerChoice, computerChoice);
-//     showWinner(winner, computerChoice);
-//   }
-
-
-
-// function showWinner(winner) {
-//     if (winner === "Player") {
-//         resultText.textContent = `Result: Player WON`;
-//         playerScore++
-//     } else if (winner === "Computer") {
-//         resultText.textContent = `Result: Computer WON`;
-//         computerScore++
-//     }
-// }
-
-// function checkWinner(playerSelection, computerSelection) {
-
-//     if (playerSelection == computerSelection) {
-//         return "It's a DRAW! Play Again!!"
-
-//     } else if (
-//         (playerSelection == 'rock' && computerSelection == 'scissors') ||
-//         (playerSelection == 'paper' && computerSelection == 'rock') || 
-//         (playerSelection == 'scissors' && computerSelection == 'paper')) {
-        
-//         return 'Player'
-        
-//     } else if (
-//         (playerSelection == 'scissors' && computerSelection == 'rock') ||
-//         (playerSelection == 'rock' && computerSelection == 'paper') || 
-//         (playerSelection == 'paper' && computerSelection == 'scissors')) {
-        
-//         return 'Computer'
-//     }
-
-// }
-
-
 function playRound(playerSelection, computerSelection) {
-    
+
     let result = {};
     if (playerSelection === computerSelection) {
         result.message = "It's a DRAW! Play Again!!"
@@ -140,15 +54,14 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'paper' && computerSelection === 'rock') || 
         (playerSelection === 'scissors' && computerSelection === 'paper')) {
-        result.message = 'You WIN';
-        result.playerScore = ++playerScore;
+        result.message = 'Player WIN';
+        result.playerScore = playerScore++;
         result.computerScore = computerScore;
         
     } else {
         result.message = 'Computer WIN';
         result.playerScore = playerScore;
-        result.computerScore = ++computerScore;
+        result.computerScore = computerScore++;
     }
-    
     return result.message;
 }
